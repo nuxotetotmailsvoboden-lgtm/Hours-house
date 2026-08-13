ee8664d6a8ee26b1cb79478bf805d723dc0f6e4a
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-08-14 00:35:03 +0500

    Update register.py

--- states/register.py
+++ states/register.py
@@ -1,6 +1,9 @@
 from aiogram.fsm.state import State, StatesGroup
 
 class RegisterState(StatesGroup):
-    full_name = State()
-    phone = State()
-    passport = State()
+    choose_name_method = State()      # выбор способа ввода имени
+    full_name_manual = State()        # ручной ввод имени
+    choose_phone_method = State()     # выбор способа ввода телефона
+    waiting_contact = State()         # ожидание контакта от Telegram
+    phone_manual = State()            # ручной ввод телефона
+    passport = State()                # загрузка паспорта
