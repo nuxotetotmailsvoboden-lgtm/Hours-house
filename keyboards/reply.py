7e37b94e9e06d1a1b753954bc328b6390a5c536c
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-08-13 23:35:34 +0500

    Update reply.py

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -32,3 +32,8 @@ def choose_phone_kb():
         ],
         resize_keyboard=True
     )
+def cancel_kb():
+    return ReplyKeyboardMarkup(
+        keyboard=[[KeyboardButton(text="❌ Отмена")]],
+        resize_keyboard=True
+    )
