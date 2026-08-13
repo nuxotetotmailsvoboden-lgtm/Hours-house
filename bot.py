b30235d4d28aad6ed30ed0893975d75705716e6f
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-08-13 23:36:41 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ from config import BOT_TOKEN
 from database import init_db
 from handlers import start, register, menu
 from handlers import admin, catalog
+from handlers import callback
 
 async def main():
     logging.basicConfig(level=logging.INFO)
@@ -22,12 +23,14 @@ async def main():
     dp.include_router(menu.router)
     dp.include_router(admin.router)
     dp.include_router(catalog.router)
+    dp.include_router(callback.router)
     
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
+
 
 
 
