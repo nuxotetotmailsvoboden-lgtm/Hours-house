1f9a02f4ffdaeee43e57a6885eb5691e0fdfcd65
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-08-13 23:27:36 +0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,11 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
+from models import Base
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+async def init_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
