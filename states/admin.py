04b2e383d3126b581a3f8db0b00c8e86e60c3e86
Name1 <nuxotetotmailsvoboden@gmail.com>
2026-08-14 11:55:36 +0500

    Update admin.py

--- states/admin.py
+++ states/admin.py
@@ -6,3 +6,12 @@ class AddApartmentState(StatesGroup):
     price_hour = State()
     price_day = State()
     photo = State()
+
+class EditApartmentState(StatesGroup):
+    choosing_field = State()          # выбор поля для редактирования
+    editing_name = State()
+    editing_description = State()
+    editing_price_hour = State()
+    editing_price_day = State()
+    editing_photo = State()
+    confirming_delete = State()       # подтверждение удаления
